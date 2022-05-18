2eb18231feec26b3d247aad80df6d7f8da4905f0
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2022-05-19 00:18:41 +0200

    added navbar

--- presto.js
+++ presto.js
@@ -1,6 +1,19 @@
 const arrowNavbar = document.querySelector('#arrowNavbar')
 const navbarExpander = document.querySelector('#navbarExpander')
+const navLink = document.querySelectorAll('.nav-link')
 
 navbarExpander.addEventListener('click', ()=>{
     arrowNavbar.classList.toggle('rotateArrow')
 })
+
+navLink.forEach(link => {
+    link.addEventListener('click',()=>{
+        link.classList.add('active')
+        navLink.forEach(el=>{
+            if(el!=link)
+                el.classList.remove('active')
+        })
+    })
+})
+
+console.log(navLink);
