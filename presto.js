4e081462dd3f56ea9678c6099c5aa9f251dbf04e
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2022-05-19 18:12:01 +0200

    starting new ads section

--- presto.js
+++ presto.js
@@ -1,9 +1,18 @@
+//Query selectors
 const arrowNavbar = document.querySelector('#arrowNavbar')
 const navbarExpander = document.querySelector('#navbarExpander')
 const navLink = document.querySelectorAll('.nav-link')
+const searchBar = document.querySelector('#searchBar')
+const searchBar2 = document.querySelector('#searchBar2')
+
+// Swiper
+// import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+// const swiper = new Swiper(...)
+
 
 navbarExpander.addEventListener('click', ()=>{
     arrowNavbar.classList.toggle('rotateArrow')
+    arrowNavbar.classList.toggle('noRotate')
 })
 
 navLink.forEach(link => {
@@ -17,3 +26,11 @@ navLink.forEach(link => {
 })
 
 console.log(navLink);
+
+searchBar.addEventListener('click', ()=>{
+    console.log('ciaoooo sono la searchBar 1');
+})
+
+searchBar2.addEventListener('click', ()=>{
+    console.log('ciaoooo sono la searchBar 2');
+})
