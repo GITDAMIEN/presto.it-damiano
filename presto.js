172bdf75b0b36882d155cf5fa5438b1b36c5d9e5
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2022-05-21 12:59:27 +0200

    set new ads carousel

--- presto.js
+++ presto.js
@@ -4,11 +4,18 @@ const navbarExpander = document.querySelector('#navbarExpander')
 const navLink = document.querySelectorAll('.nav-link')
 const searchBar = document.querySelector('#searchBar')
 const searchBar2 = document.querySelector('#searchBar2')
+const heartBtn = document.querySelector('#heartBtn')
+const cartBtn = document.querySelector('#cartBtn')
 
-// Swiper
-// import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
-// const swiper = new Swiper(...)
+// Variables
 
+
+//Problems to be solved
+// 1- heart gets covered by its background after click (??)
+// 2- add an initial section with a quick website description
+
+
+//Event listeners
 
 navbarExpander.addEventListener('click', ()=>{
     arrowNavbar.classList.toggle('rotateArrow')
@@ -33,4 +40,18 @@ searchBar.addEventListener('click', ()=>{
 
 searchBar2.addEventListener('click', ()=>{
     console.log('ciaoooo sono la searchBar 2');
+})
+
+heartBtn.addEventListener('mouseenter', ()=>{
+    heartBtn.innerHTML=''
+    heartBtn.innerHTML=`<i class="fa-solid fa-heart"></i>`
+})
+
+heartBtn.addEventListener('mouseleave', ()=>{
+    heartBtn.innerHTML=''
+    heartBtn.innerHTML=`<i class="fa-regular fa-heart"></i>`
+})
+
+heartBtn.addEventListener('click', ()=>{
+    heartBtn.classList.toggle('back-sec')
 })
