33592a11f95ac81645173a8943098b744e25d7d3
Damiano Bellamoli <damiano.bellamoli@hotmail.it>
2022-05-21 16:55:42 +0200

    added easter egg :)

--- presto.js
+++ presto.js
@@ -1,19 +1,59 @@
+
+//Problems to be solved
+// 1- heart gets covered by its background after click (??)
+// 2- add an initial section with a quick website description
+
 //Query selectors
 const arrowNavbar = document.querySelector('#arrowNavbar')
 const navbarExpander = document.querySelector('#navbarExpander')
 const navLink = document.querySelectorAll('.nav-link')
 const searchBar = document.querySelector('#searchBar')
 const searchBar2 = document.querySelector('#searchBar2')
-const heartBtn = document.querySelector('#heartBtn')
-const cartBtn = document.querySelector('#cartBtn')
+const heartBtn = document.querySelectorAll('#heartBtn')
+const cartBtn = document.querySelectorAll('#cartBtn')
+const categoriesText = document.querySelector('#categoriesText')
+const easterEggToastMessage = document.querySelector('#easterEggToastMessage')
 
 // Variables
+var easterEggCounter = 0;
 
+//Swiper
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-//Problems to be solved
-// 1- heart gets covered by its background after click (??)
-// 2- add an initial section with a quick website description
-
+    //breakpoints
+    breakpoints: {
+        700: {
+            slidesPerView: 2,
+            // spaceBetween: 20,
+        },
+        900: {
+            slidesPerView: 3,
+            // spaceBetween: 30,
+        },
+        1200: {
+            slidesPerView: 4,
+            // spaceBetween: 50,
+        },
+    }
+    
+    // And if we need scrollbar
+    // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+});
 
 //Event listeners
 
@@ -42,16 +82,52 @@ searchBar2.addEventListener('click', ()=>{
     console.log('ciaoooo sono la searchBar 2');
 })
 
-heartBtn.addEventListener('mouseenter', ()=>{
-    heartBtn.innerHTML=''
-    heartBtn.innerHTML=`<i class="fa-solid fa-heart"></i>`
+heartBtn.forEach(btn=>{
+    btn.addEventListener('mouseenter', ()=>{
+        btn.innerHTML=''
+        btn.innerHTML=`<i class="fa-solid fa-heart"></i>`
+    })  
 })
 
-heartBtn.addEventListener('mouseleave', ()=>{
-    heartBtn.innerHTML=''
-    heartBtn.innerHTML=`<i class="fa-regular fa-heart"></i>`
+heartBtn.forEach(btn=>{
+    btn.addEventListener('mouseleave', ()=>{
+        btn.innerHTML=''
+        btn.innerHTML=`<i class="fa-regular fa-heart"></i>`
+    })  
 })
 
-heartBtn.addEventListener('click', ()=>{
-    heartBtn.classList.toggle('back-sec')
+heartBtn.forEach(btn=>{
+    btn.addEventListener('click', ()=>{
+        btn.classList.toggle('back-sec')
+    })  
+})
+
+//Functions
+function displayToastMessage(message){
+    let toast = document.createElement('div')
+    toast.classList.add('d-flex')
+    toast.innerHTML=`
+        <div class="toast-body">
+            ${message}
+        </div>
+        <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close">
+        </button>
+      `
+    easterEggToastMessage.innerHTML=''
+    easterEggToastMessage.appendChild(toast);
+}
+
+//Easter Egg
+
+categoriesText.addEventListener('click', ()=>{
+    easterEggCounter++;
+    console.log('toast');
+    console.log(easterEggToastMessage);
+    if(easterEggCounter>=10){
+        displayToastMessage('Stop pushing that button! :)')
+        easterEggToastMessage.classList.add('fade','show')
+    }else if(easterEggCounter==5){
+        displayToastMessage('Hey! You just found an easter egg! :)')
+        easterEggToastMessage.classList.add('fade','show')
+    }
 })
